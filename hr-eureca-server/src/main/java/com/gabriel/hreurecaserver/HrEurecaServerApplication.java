08f410fcf14a156e0c408cfac852376e02d80ce2
Gabriel <silva.gabrielmelosantos@gmail.com>
2023-06-26 19:32:18 -0300

    hr-eureka-server criado

---
+++ hr-eureca-server/src/main/java/com/gabriel/hreurecaserver/HrEurecaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.hreurecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurecaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurecaServerApplication.class, args);
+	}
+
+}
--- hr-payroll/src/main/java/com/gabriel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/gabriel/hrpayroll/services/PaymentService.java
@@ -1,10 +1,6 @@
 package com.gabriel.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.hrpayroll.entities.Payment;
